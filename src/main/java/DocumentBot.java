9694c0235a09d7d60d612af89898219e16e93c94
Ramz3301 <ramkurbanov10@yandex.ru>
2022-06-29 09:27:18 +0300

    ad logic for create file and write into this file

--- src/main/java/DocumentBot.java
+++ src/main/java/DocumentBot.java
@@ -1,11 +1,10 @@
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendDocument;
-import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import java.io.File;
+import java.io.*;
 
 public class DocumentBot extends TelegramLongPollingBot {
 
@@ -25,7 +24,22 @@ public class DocumentBot extends TelegramLongPollingBot {
         if (update.hasMessage() && update.getMessage().hasText()) {
             SendDocument document = new SendDocument();
             document.setChatId("-1001579511149L");
-            document.setDocument(new InputFile(new File("/home/ramz/Documents/demo.pdf")));
+
+            /**
+             * Create file and write into this something
+             */
+            File file = new File("/home/ramz/Documents/demo.txt");
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))){
+                file.createNewFile();
+                writer.write("Hello dude");
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+
+//            document.setDocument(new InputFile(new File("/home/ramz/Documents/demo.pdf")));
+            document.setDocument(new InputFile(file));
 
             try {
                 execute(document); // Call method to send the message
