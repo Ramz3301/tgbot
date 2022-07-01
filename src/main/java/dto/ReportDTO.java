d28cb45969b9617aa9ba43ac4af328a8a9eaf670
Ramz3301 <ramkurbanov10@yandex.ru>
2022-07-01 11:44:42 +0300

    add dtos

---
+++ src/main/java/dto/ReportDTO.java
@@ -0,0 +1,18 @@
+package dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class ReportDTO {
+    private LocalDate date;
+    List<TeamReportDTO> teamReports;
+}
